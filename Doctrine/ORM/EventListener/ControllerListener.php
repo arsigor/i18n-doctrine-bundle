31b5e048a471f14a5d9ab0342b5b19c620cc8d8a
ars <arsenych.igor@gmail.com>
2020-01-15 19:45:01 +0200

    test commit

--- Doctrine/ORM/EventListener/ControllerListener.php
+++ Doctrine/ORM/EventListener/ControllerListener.php
@@ -3,7 +3,7 @@
 namespace arsigor\I18nDoctrineBundle\Doctrine\ORM\EventListener;
 
 use arsigor\I18nDoctrineBundle\EventListener\ControllerListener as BaseControllerListener,
-    Symfony\Component\HttpKernel\Event\FilterControllerEvent,
+    Symfony\Component\HttpKernel\Event\ControllerEvent,
     Doctrine\Common\Util\ClassUtils;
 
 /**
@@ -18,28 +18,29 @@ class ControllerListener extends BaseControllerListener
      * @param \Symfony\Component\HttpKernel\Event\FilterControllerEvent $event
      * @return type
      */
-    public function onKernelController(FilterControllerEvent $event)
+    public function onKernelController(ControllerEvent $event)
     {
-        $controller = $event->getController();
-        list($object, $method) = $controller;
-
-        $className = ClassUtils::getClass($object);
-        $reflectionClass = new \ReflectionClass($className);
-
-        // Sonata
-        $sonataAdmin = 'Sonata\AdminBundle\Controller\CRUDController';
-        if (class_exists($sonataAdmin) && ($sonataAdmin === $className || $reflectionClass->isSubclassOf($sonataAdmin)) && in_array($method, array('createAction', 'editAction'))) {
-            $this->om->getFilters()->disable('oneLocale');
-            return;
-        }
-
-        $reflectionMethod = $reflectionClass->getMethod($method);
-        if ($this->annotationReader->getMethodAnnotation($reflectionMethod, 'arsigor\I18nDoctrineBundle\Annotation\I18nDoctrine')) {
-            $this->om->getFilters()->disable('oneLocale');
-
-        } else {
-            $this->om->getFilters()->enable('oneLocale')->setParameter('locale', $event->getRequest()->getLocale());
-        }
+        //@TODO fix this
+//        $controller = $event->getController();
+//        list($object, $method) = $controller;
+//
+//        $className = ClassUtils::getClass($object);
+//        $reflectionClass = new \ReflectionClass($className);
+//
+//        // Sonata
+//        $sonataAdmin = 'Sonata\AdminBundle\Controller\CRUDController';
+//        if (class_exists($sonataAdmin) && ($sonataAdmin === $className || $reflectionClass->isSubclassOf($sonataAdmin)) && in_array($method, array('createAction', 'editAction'))) {
+//            $this->om->getFilters()->disable('oneLocale');
+//            return;
+//        }
+//
+//        $reflectionMethod = $reflectionClass->getMethod($method);
+//        if ($this->annotationReader->getMethodAnnotation($reflectionMethod, 'arsigor\I18nDoctrineBundle\Annotation\I18nDoctrine')) {
+//            $this->om->getFilters()->disable('oneLocale');
+//
+//        } else {
+//            $this->om->getFilters()->enable('oneLocale')->setParameter('locale', $event->getRequest()->getLocale());
+//        }
     }
 
 }
